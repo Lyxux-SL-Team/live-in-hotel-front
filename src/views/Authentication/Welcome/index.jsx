f158a1da74deaa3a08c7e4d5c595a093ce5462dc
AronDevpro <140414272+AronDevpro@users.noreply.github.com>
2024-07-25 18:03:53 +0530

    modify the welcome screen

--- src/views/Authentication/Welcome/index.jsx
+++ src/views/Authentication/Welcome/index.jsx
@@ -1,44 +1,32 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import {Button, Col, Container, Row} from "react-bootstrap";
 import CommanFooter1 from "../CommanFooter1";
-import bgVideo from "../../../assets/video/signupBg.mp4";
+import loginImage from "../../../assets/img/login-img.png";
+import {useSelector} from "react-redux";
+import {colors as Colors} from "../../../configs/colors.js";
+import {Link} from "react-router-dom";
 
 function Welcome() {
+
+  const user = useSelector(state => state.auth.user);
   return (
-    <div className="hk-pg-wrapper py-0">
-      <div className="hk-pg-body py-0">
-        <Container fluid>
-          <Row className="auth-split">
-            <Col
-              xl={5}
-              lg={6}
-              md={5}
-              className="p-0 d-md-block d-none position-relative overflow-hidden"
-            >
-              <video className="bg-img" autoPlay loop muted src={bgVideo} />
-            </Col>
-            <Col
-              xl={7}
-              lg={6}
-              md={7}
-              sm={10}
-              className="position-relative mx-auto d-flex justify-content-center align-items-center flex-column"
-            >
-              <div
-                className="d-flex justify-content-evenly flex-column gap-3"
-                style={{ maxWidth: "600px" }}
-              >
-                <p className="text-black fw-bolder fs-2 font-scale-vf">
-                  Welcome to LiveInHotel
-                </p>
-                <p className="fw-medium">
-                  We'll send an email to dvdfvh@hhdv.com to verify your account
+      <Container fluid className="hk-pg-wrapper py-0">
+        <Row className="auth-split">
+          <Col xl={5}
+               lg={5}
+               md={5}
+               className="p-0 d-md-block d-none position-relative overflow-hidden d-sm-none" style={{backgroundColor:"rgba(0, 0, 0, 0.32)"}}>
+            <img src={loginImage} alt="live in hotel" className="bg-img" />
+          </Col>
+            <Col sm={12} xs={12} md={7} lg={7} className="position-relative mx-auto">
+              <div className="mx-5 mx-lg-10 mx-xl-15 pt-15 pt-lg-20">
+                <h4 className="mb-3 font-scale-vf" style={{color:Colors.Dark}}>Welcome to LiveInHotel</h4>
+                <p className="mb-3" style={{color:Colors.Dark, fontSize:16}}>
+                  We'll send an email to {user.email} to verify your account
                   and let you know what to expect next.
                 </p>
-                <p className="text-black fs-5 fw-bold">
-                  To build your listing you'll need:
-                </p>
-                <ul style={{ listStyleType: "disc" }}>
+                <h6 className="mb-3" style={{color:Colors.Dark, fontSize:18, fontWeight:500}}>To build your listing you'll need:</h6>
+                <ul className="mb-4" style={{ listStyleType: "disc", color:Colors.Dark, fontSize:16 }}>
                   <li>Pictures of your property</li>
                   <li>
                     Information about your rooms and the amenities you offer
@@ -49,19 +37,25 @@ function Welcome() {
                     government
                   </li>
                 </ul>
-                <p className="text-black fs-5 fw-bold">Unsure if you have enough</p>
-                <p>
+                <h6 className="mb-3" style={{color:Colors.Dark, fontSize:18, fontWeight:500}}>Unsure if you have enough</h6>
+                <p style={{color:Colors.Dark, fontSize:18}}>
                   No need to worry! We'll guide you through each step, and
                   you'll have the opportunity to edit your listing before it
                   goes live.
                 </p>
+                <Button
+                    style={{backgroundColor:Colors.Dark, fontSize:16, fontWeight:500}}
+                    className="btn-rounded col-11 mt-5"
+                    as={Link}
+                    to='/auth/signup-step-4'
+                >
+                  Continue
+                </Button>
               </div>
               <CommanFooter1 />
             </Col>
-          </Row>
-        </Container>
-      </div>
-    </div>
+        </Row>
+      </Container>
   );
 }
 
