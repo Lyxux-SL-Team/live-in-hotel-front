23e97331f7ace0ecf59df17f1189f884c576e393
AronDevpro <140414272+AronDevpro@users.noreply.github.com>
2024-07-25 17:57:27 +0530

    add new colors, add new dependency zod and formik, add new images

---
+++ src/configs/colors.js
@@ -0,0 +1,18 @@
+export const colors ={
+    FooterBlue:"#02BCBC",
+    Black:"#000000",
+    Dark:"#1A1A1A",
+    Dark1:"#323232",
+    Grey:"#878787",
+    Grey2:"#9C9C9C",
+    Grey3:"#EBEBEB",
+    Grey4:"#666666",
+    Grey5:"#C6C6C6",
+    Grey6:"#BABABA",
+    white:"#fff",
+    white1:"#DBDBDB",
+    white2:"#E7E7E7",
+    white3:"#EAEAEA",
+    Purple:"#93278F",
+    Green:"#02BC091A",
+}
